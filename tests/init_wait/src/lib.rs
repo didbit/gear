4e1de0e56d272be34ad5e3d04c21fdc0c354b66e
Georgy Shepelev <geo.shepelev@gmail.com>
2021-12-03 13:38:33 +0400

    add tests/init-wait
    split examples/init-wait/lib.rs into two files: one with crate level
    'no_std' attribute and code.rs with the code. This trick allows to use
    code.rs in tests/init-wait/lib.rs.

---
+++ tests/init_wait/src/lib.rs
@@ -0,0 +1,39 @@
+#![cfg_attr(not(feature = "std"), feature(alloc_error_handler))]
+#![cfg_attr(not(feature = "std"), feature(const_btree_new))]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "std")]
+#[cfg(test)]
+mod native {
+    include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+}
+
+#[cfg(not(feature = "std"))]
+mod wasm {
+    include!{"../../../examples/init-wait/src/code.rs"}
+}
+
+#[cfg(test)]
+#[cfg(feature = "std")]
+mod tests {
+    use super::{native};
+    use common::*;
+
+    #[test]
+    fn binary_available() {
+        assert!(native::WASM_BINARY.is_some());
+        assert!(native::WASM_BINARY_BLOATY.is_some());
+    }
+
+    fn wasm_code() -> &'static [u8] {
+        native::WASM_BINARY_BLOATY.expect("wasm binary exists")
+    }
+
+    #[test]
+    fn waiting_program_initialization() {
+        let mut runner = RunnerContext::default();
+
+        let report: RunReport<()> = runner.init_program_with_report(wasm_code());
+        assert!(matches!(report.result, RunResult::Waiting));
+    }
+}
